843537b194ecc83272b5b2ce846fca8c0fb53fcf
Andrea Parodi <andrea@parro.it>
2017-11-17 20:20:33 +0100

    readme

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import AsyncIterable from "asynciterable";
 
+/**
+ * Create an async iterable from a stream
+ * @param  {ReadableStream} stream the stream to transform
+ * @return {AsyncIterable} converted iterator
+ */
 export default function aiFromStream(stream) {
   return new AsyncIterable((write, end, error) => {
     stream.on("data", write);
